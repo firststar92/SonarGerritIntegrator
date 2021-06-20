dbdf25e6a6ad0e1b4d4128c94159ff1d0983fb94
MSDehghan <msdehghan77@gmail.com>
2021-06-20 18:18:09 +0430

    Implement Sonar issues retrieval

--- src/main/java/ir/sahab/sonar/gerrit/integrator/dto/SonarSearchResult.java
+++ src/main/java/ir/sahab/sonar/gerrit/integrator/dto/SonarSearchResult.java
@@ -1,4 +1,48 @@
 package ir.sahab.sonar.gerrit.integrator.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class SonarSearchResult {
+    @NotNull
+    public Integer total;
+
+    @NotNull
+    public List<Issue> issues;
+
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Issue {
+        public String key;
+        public Integer line;
+        public TextRange textRange;
+
+        @NotNull
+        public String rule;
+
+        @NotNull
+        public String severity;
+
+        @NotNull
+        public String component;
+
+        @NotNull
+        public String message;
+    }
+
+    public static class TextRange {
+        @NotNull
+        public Integer startLine;
+
+        @NotNull
+        public Integer endLine;
+
+        @NotNull
+        public Integer startOffset;
+
+        @NotNull
+        public Integer endOffset;
+    }
 }
--- src/main/java/ir/sahab/sonar/gerrit/integrator/service/SonarQubeService.java
+++ src/main/java/ir/sahab/sonar/gerrit/integrator/service/SonarQubeService.java
@@ -1,13 +1,15 @@
 package ir.sahab.sonar.gerrit.integrator.service;
 
+import ir.sahab.sonar.gerrit.integrator.dto.SonarSearchResult;
 import ir.sahab.sonar.gerrit.integrator.dto.SonarWebHook;
 import ir.sahab.sonar.gerrit.integrator.dto.SonarWebHook.Branch.BranchType;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.support.BasicAuthenticationInterceptor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 public class SonarQubeService {
@@ -21,11 +23,15 @@ public class SonarQubeService {
         this.sonarAddress = sonarAddress;
     }
 
-    public void getIssuesOf(SonarWebHook webHook) {
+    public SonarSearchResult getIssuesOf(SonarWebHook webHook) {
         String type = webHook.branch.type == BranchType.BRANCH ? "branch" : "pullRequest";
         final String url = String.format("%s/api/issues/search?%s=%s&componentKeys=%s", sonarAddress, type,
                 webHook.branch.name, webHook.project.key);
-        ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-        System.out.println(response.getBody());
+        ResponseEntity<SonarSearchResult> response = restTemplate.getForEntity(url, SonarSearchResult.class);
+        if (!response.getStatusCode().is2xxSuccessful()) {
+            throw new ResponseStatusException(HttpStatus.FAILED_DEPENDENCY,
+                    "SonarQube responded: " + response.getStatusCode());
+        }
+        return response.getBody();
     }
 }
